25740d3b236030adf3e6719a7618294dfc703921
Robert Wise <robcodehub@gmail.com>
2019-09-17 04:48:38 +0200

    Created person in sequelize

--- db.js
+++ db.js
@@ -2,9 +2,26 @@ const Sequelize = require('sequelize');
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_nouns');
 
+const Person = conn.define('person', {
+  id: {
+    primaryKey: true,
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  }
+});
 
 
 
 
+module.exports = {
+  models: {
+    Person
+  }
+}
 
 
